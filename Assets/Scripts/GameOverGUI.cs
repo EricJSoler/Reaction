a517408a488648a5220b070d0dc79ad0c8dd0de8
Devin Stoen <Devin Stoen>
2016-11-26 13:06:56 -0800

    designs pretty much done, leaderboard bug fix

--- Assets/Scripts/GameOverGUI.cs
+++ Assets/Scripts/GameOverGUI.cs
@@ -8,11 +8,14 @@ public class GameOverGUI : MonoBehaviour
     public Texture btnTexture;
     public GUIStyle TextStyle;
     public GUIStyle TextStyle2;
+    
     public GUIStyle btnStyle;
     public GUIStyle btnStyle2;
     public GUIStyle btnStyle3;
     public GameObject saveScore;
     ScoresManager scores_m;
+    bool once = false;
+    public HighScore[] HighScoreList;
 
     int curScore = 0;
 
@@ -21,7 +24,7 @@ public class GameOverGUI : MonoBehaviour
         saveScore = GameObject.Find("ScoreSave");
         scores_m = saveScore.GetComponent<ScoresManager>();
         curScore = scores_m.getcurScore();
-        
+        once = false;
 
     }
     
@@ -47,40 +50,53 @@ public class GameOverGUI : MonoBehaviour
         TextStyle.fontSize = (int)(160.0f * (float)(Screen.width) / 1920.0f); //scale size font
         TextStyle2.fontSize = (int)(80.0f * (float)(Screen.width) / 1920.0f); //scale size font
 
+        if (scores_m.arReady())
+        {        
+             HighScoreList = scores_m.getHighScoreList();
+        }
+        
     }
 
     void OnGUI()
     {
 
-        GUI.Label(new Rect(Screen.width / 4.2f, Screen.height / 5.25f, Screen.width / 6, Screen.width / 6), "Score: ", TextStyle);
+        GUI.Label(new Rect(Screen.width / 4.2f, Screen.height / 5.25f, Screen.width / 6, Screen.width / 6), "score: ", TextStyle);
         GUI.Label(new Rect(Screen.width / 1.8f, Screen.height / 5.25f, Screen.width / 6, Screen.width / 6), curScore.ToString(), TextStyle);
 
-        GUI.Label(new Rect(Screen.width / 4.2f, Screen.height / 3.5f, Screen.width / 6, Screen.width / 6), "Best: ", TextStyle);
+        GUI.Label(new Rect(Screen.width / 4.2f, Screen.height / 3.5f, Screen.width / 6, Screen.width / 6), "best: ", TextStyle);
 
-        GUI.Label(new Rect(Screen.width / 1.8f, Screen.height / 3.7f, Screen.width / 6, Screen.width / 6), 
+        GUI.Label(new Rect(Screen.width / 1.8f, Screen.height / 3.5f, Screen.width / 6, Screen.width / 6), 
             scores_m.getHighScore().ToString()
             , TextStyle);
 
+        GUI.Label(new Rect(Screen.width / 8f, Screen.height / 2.63f, Screen.width / 6, Screen.width / 6), "world record: ", TextStyle);
+
+        if ((HighScoreList != null))
+        {
+            GUI.Label(new Rect(Screen.width / 1.35f, Screen.height / 2.63f, Screen.width / 6, Screen.width / 6),
+                HighScoreList[0].score.ToString()
+                , TextStyle);
+        }
         //GUI.Label(new Rect(Screen.width / 4.2f, Screen.height / 2.8f, Screen.width / 6, Screen.width / 6), "Rank: ", TextStyle);
-        
+
 
         //GUI.Label(new Rect(Screen.width / 4.2f, Screen.height / 1.3f, Screen.width / 6, Screen.width / 6), "name test: " + scores_m.getUsername().ToString(), TextStyle2);
 
         //GUI.Label(new Rect(10, 10, 100, 40), "GameOver");
-        if (GUI.Button(new Rect(Screen.width / 4, Screen.height / 2, Screen.width / 2, Screen.height / 11), "", btnStyle))
+        if (GUI.Button(new Rect(Screen.width / 4, Screen.height / 2f, Screen.width / 2, Screen.height / 10.2f), "", btnStyle))
         {
             scores_m.setcurScore(0);
             SceneManager.LoadScene(1);
         }
 
         
-        if (GUI.Button(new Rect(Screen.width / 4, Screen.height / 1.6f, Screen.width / 2, Screen.height / 11), "", btnStyle2))
+        if (GUI.Button(new Rect(Screen.width / 4, Screen.height / 1.58f, Screen.width / 2, Screen.height / 10.2f), "", btnStyle2))
         {
             scores_m.setcurScore(0);
             SceneManager.LoadScene(0);
         }
 
-        if (GUI.Button(new Rect(Screen.width / 4, Screen.height / 1.3f, Screen.width / 2, Screen.height / 11), "", btnStyle3))
+        if (GUI.Button(new Rect(Screen.width / 4, Screen.height / 1.30f, Screen.width / 2, Screen.height / 10.2f), "", btnStyle3))
         {
             //scores_m.setcurScore(0);
             SceneManager.LoadScene(3);
